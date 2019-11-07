a83c059b6b4335dbfec0dc1147f72b569b36b359
Pedro <Pedro.lin.ka@gmai.com>
2019-11-07 13:47:11 -0500

    commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { InventoryService } from '../services/inventory.service';
 import {  NgForm } from '@angular/forms'
+import { Router, ActivatedRoute } from '@angular/router'
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -8,14 +9,19 @@ import {  NgForm } from '@angular/forms'
 })
 
 export class UsersComponent implements OnInit {
-  constructor( private inventory: InventoryService) {}
+  constructor( private inventory: InventoryService,
+               private router: Router,
+               private route: ActivatedRoute) {}
   ngOnInit() {}
 
   onSubmit(form: NgForm) {
     this.inventory.users.push(form.value);
     console.log(form.value);
   }
- 
+  
+  reloadPage() {
+    this.router.navigate(['doors'], {relativeTo: this.route }) // this will add doors to the currently activated route
+  }
 
 
 }
